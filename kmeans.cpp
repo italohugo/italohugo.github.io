a0ee1f118f3f5126dc9d0cca757fbaa4e013c5df
italohugo <italocosta16@gmail.com>
2018-05-20 22:00:58 -0300

    att4

--- kmeans.cpp
+++ kmeans.cpp
@@ -6,24 +6,23 @@ using namespace cv;
 int main( int argc, char** argv ){
   int nClusters = 6;
   Mat rotulos;
-  int nRodadas = 5;
+  int nRodadas = 1;
   Mat centros;
 
-  if(argc!=3){
-	exit(0);
-  }
-  
-  Mat img = imread( argv[1], CV_LOAD_IMAGE_COLOR);
-  Mat samples(img.rows * img.cols, 3, CV_32F);
 
-  for( int y = 0; y < img.rows; y++ ){
-    for( int x = 0; x < img.cols; x++ ){
-      for( int z = 0; z < 3; z++){
-        samples.at<float>(y + x*img.rows, z) = img.at<Vec3b>(y,x)[z];
-	  }
-	}
-  }
-  
+  Mat img = imread( "gash.jpg", CV_LOAD_IMAGE_COLOR);
+  Mat samples(img.rows * img.cols, 3, CV_32F);
+    for (int i = 0; i < 10; i++)
+    {
+      for( int y = 0; y < img.rows; y++ ){
+        for( int x = 0; x < img.cols; x++ ){
+          for( int z = 0; z < 3; z++){
+            samples.at<float>(y + x*img.rows, z) = img.at<Vec3b>(y,x)[z];
+          }
+        }
+      }
+
+
   kmeans(samples,
 		 nClusters,
 		 rotulos,
@@ -35,14 +34,54 @@ int main( int argc, char** argv ){
 
   Mat rotulada( img.size(), img.type() );
   for( int y = 0; y < img.rows; y++ ){
-    for( int x = 0; x < img.cols; x++ ){ 
+    for( int x = 0; x < img.cols; x++ ){
 	  int indice = rotulos.at<int>(y + x*img.rows,0);
 	  rotulada.at<Vec3b>(y,x)[0] = (uchar) centros.at<float>(indice, 0);
 	  rotulada.at<Vec3b>(y,x)[1] = (uchar) centros.at<float>(indice, 1);
 	  rotulada.at<Vec3b>(y,x)[2] = (uchar) centros.at<float>(indice, 2);
 	}
   }
-  imshow( "clustered image", rotulada );
-  imwrite(argv[2], rotulada);
-  waitKey( 0 );
+  //salvando as imagens
+		if (i == 0) {
+			imshow("clustered image", rotulada);
+			imwrite("k1.jpg", rotulada);
+		}
+		if (i == 1) {
+			imshow("clustered image", rotulada);
+			imwrite("k2.jpg", rotulada);
+		}
+		if (i == 2) {
+			imshow("clustered image", rotulada);
+			imwrite("k3.jpg", rotulada);
+		}
+		if (i == 3) {
+			imshow("clustered image", rotulada);
+			imwrite("k4.jpg", rotulada);
+		}
+		if (i == 4) {
+			imshow("clustered image", rotulada);
+			imwrite("k5.jpg", rotulada);
+		}
+		if (i == 5) {
+			imshow("clustered image", rotulada);
+			imwrite("k6.jpg", rotulada);
+		}
+		if (i == 6) {
+			imshow("clustered image", rotulada);
+			imwrite("k7.jpg", rotulada);
+		}
+		if (i == 7) {
+			imshow("clustered image", rotulada);
+			imwrite("k8.jpg", rotulada);
+		}
+		if (i == 8) {
+			imshow("clustered image", rotulada);
+			imwrite("k9.jpg", rotulada);
+		}
+		if (i == 9) {
+			imshow("clustered image", rotulada);
+			imwrite("k10.jpg", rotulada);
+		}
+  }
+  return 0;
 }
